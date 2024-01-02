d34be1f9a3d9e1aafb47d78b21c113cc2fbb5ce3
patrikpecenkae <ppecenka@icloud.com>
2024-01-02 19:49:00 +0100

    feature: implement new scroll code

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,24 +1,27 @@
 import cvPdf from "/Resume-CZ-ENG.pdf"
 import useShowComponent from "hooks/useShowComponent"
 //import { IconBrandGithub, } from '@tabler/icons-react'
+import { useTranslation } from "react-i18next"
 import SkillSet from "./SkillSet"
 
 const AboutSection = () => {
 
   useShowComponent({ selector: ".about-section-wrapper"})
 
+  const [t] = useTranslation("translation")
+
   return (
     <div id="about" className="about-section-wrapper hidden-about">  
       <div className="about-section-container">
         <h1 className="about-section-title">
-          My Skills          
+          {t("about_section.title")}         
         </h1>
         <SkillSet/>
         <div className="about-section-text">
         I like to craft solid and scalable frontend products with great user experiences.
         </div>
         <a href={cvPdf} rel="noreferrer" target="_blank">
-          <button className="button download-cv">Download CV</button>
+          <button className="button download-cv">{t("about_section.button_cv")}</button>
         </a>
       </div>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from 'pages/App';
 import i18n from 'i18n'
 import { I18nextProvider } from 'react-i18next';
+import Lenis from '@studio-freight/lenis'
 
 import 'main.scss'
 
+const lenis = new Lenis()
+
+lenis.on('scroll', (e: any) => {
+  console.log(e)
+})
+
+function raf(time: any) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
